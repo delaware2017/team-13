1dc0399fe317b28246da75fda68bfecb54c07559
nlin1 <nathanlin124@gmail.com>
2017-11-04 03:55:59 -0400

    fixed log-in screen dead end

--- flask-logins/app.py
+++ flask-logins/app.py
@@ -12,7 +12,7 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "Hello Boss!"
+        return render_template('application.html')
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
