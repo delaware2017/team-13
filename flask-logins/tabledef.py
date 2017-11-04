64a3dc422a1ba54f631e848226e11fdfad37d5f6
Atharva Bhat <abhat98@gmail.com>
2017-11-04 02:01:47 -0400

    added tabledef.py

---
+++ flask-logins/tabledef.py
@@ -0,0 +1,38 @@
+from sqlalchemy import *
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+engine = create_engine('sqlite:///users.db', echo=True)
+Base = declarative_base()
+
+########################################################################
+class User(Base):
+    """"""
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+    firstName = Column(String)
+    lastName = Column(String)
+    phoneNumber = Column(String)
+    email = Column(String)
+    personalStatement = Column(String)
+
+
+    #----------------------------------------------------------------------
+    def __init__(self, username, password, firstName, lastName, phoneNumber, email, personalStatement):
+        """"""
+        self.username = username
+        self.password = password
+        self.firstName = firstName
+        self.lastName = lastName
+        self.phoneNumber = phoneNumber
+        self.email = email
+        self.personalStatement = personalStatement
+
+
+# create tables
+Base.metadata.create_all(engine)
